45386cd87b679cb7292aa80facdebadd33ec9b96
nakamori1024 <79179090+nakamori1024@users.noreply.github.com>
2025-04-26 10:59:11 +0900

    ホバー時にマウスカーソルを変更する

--- location-app/main.js
+++ location-app/main.js
@@ -274,7 +274,21 @@ map.on('load', () => {
   });
   map.addControl(opacityControl, 'top-left');
 
-
+  map.on('mousemove', (e) => {
+    const features = map.queryRenderedFeatures(e.point, {
+      layers: [
+        'skhb-1-layer',
+        'skhb-2-layer',
+        'skhb-3-layer',
+        'skhb-4-layer'
+      ]
+    })
+    if (features.length > 0) {
+      map.getCanvas().style.cursor = 'pointer';
+    } else {
+      map.getCanvas().style.cursor = '';
+    }
+  });
 
   map.on('click', (e) => {
     const features = map.queryRenderedFeatures(e.point, {
